4f642fdcdafb127659435a2735019e1f5d66f0d5
tzouvanas <ktzuvan.personal@gmail.com>
2020-06-18 13:25:07 +0300

    Add match score calculate in alighment base class

--- matrices/methods/AlignmentMethod.py
+++ matrices/methods/AlignmentMethod.py
@@ -1,3 +1,4 @@
+from matrices.lookupTables.LookupWrapper import LookupWrapper
 class AlignmentMethod:
     
     def __init__(self,d, lookupTable:dict):
@@ -9,3 +10,16 @@ class AlignmentMethod:
 
     def init(self):
         raise Exception("Abstract method")
+
+    def calculateMatchingScore(self, x, y):
+        
+        if self.lookupTable is None:
+            if x == y:
+                return 1
+            return -1
+
+        lookupWrapper = LookupWrapper(self.lookupTable)
+        value = lookupWrapper.get(x, y)
+        
+        return value
+
